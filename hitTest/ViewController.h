94aa8c9be3bfa4055cc202152deb5a8b55ea4eac
renjwjx <renjwjx@gmail.com>
2016-09-23 22:43:27 +0800

    Initial Commit

---
+++ hitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hitTest
+//
+//  Created by jinren on 9/23/16.
+//  Copyright © 2016 jinren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hitTest
+//
+//  Created by jinren on 9/23/16.
+//  Copyright © 2016 jinren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
